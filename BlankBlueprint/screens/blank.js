bc788e237978b9d719246a7fb61b213dfd835236
TAN DUONG <tanduong@live.com>
2020-09-29 08:30:05 -0500

    add CheckBox, Radio import

--- BlankBlueprint/screens/blank.js
+++ BlankBlueprint/screens/blank.js
@@ -13,6 +13,7 @@ import {
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Slider from '@react-native-community/slider';
+import { CheckBox } from 'react-native-elements';
 import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
 
 export default class Blank extends React.Component {
