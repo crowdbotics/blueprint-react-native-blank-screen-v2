a09804d32ef21f80cb5621211793c15de6240bf9
TAN DUONG <tanduong@live.com>
2021-02-16 11:58:58 -0600

    PLAT-5256: Update RN navigation to support RN web

--- BlankBlueprint/navigator.js
+++ BlankBlueprint/navigator.js
@@ -1,12 +1,21 @@
-import { createStackNavigator } from "react-navigation-stack";
+import React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import { SlideMenuIcon } from "../../navigator/slideMenuIcon"
+import Screen from "./screens/index"
 
-import Blank from "./screens/blank";
+const Stack = createStackNavigator()
 
-export default BlankBlueprintNavigator = createStackNavigator(
-  {
-    Blank: { screen: Blank }
-  },
-  {
-    initialRouteName: "Blank"
-  }
-);
+export default () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Screen"
+        component={Screen}
+        options={({ navigation }) => ({
+          headerLeft: () => <SlideMenuIcon navigation={navigation} />,
+          headerTitle: ""
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
